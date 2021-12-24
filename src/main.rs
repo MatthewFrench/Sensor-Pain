2405522b8eb69fb1ae86067fc5db67bbbd45fd50
MatthewFrench <9086152+MatthewFrench@users.noreply.github.com>
2021-12-23 19:26:05 -0600

    Got the floor sensor working via a ball joint

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ mod physics;
 use macroquad::prelude::*;
 use physics::nphysics;
 use physics::rapier2d;
+use physics::rapier2d_joint;
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
-    nphysics::progress().await;
+    rapier2d_joint::progress().await;
+    //nphysics::progress().await;
     // Change the above line to test the other engine
     //rapier2d::progress().await;
 }
--- src/physics/mod.rs
+++ src/physics/mod.rs
@@ -1,2 +1,3 @@
 pub mod rapier2d;
 pub mod nphysics;
+pub mod rapier2d_joint;
